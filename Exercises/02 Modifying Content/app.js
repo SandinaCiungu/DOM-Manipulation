71d5c5ff61c3d297468c70de165d2906687a82fc
Sandina Ciungu <cornelia.ocheana@farmexim.ro>
2023-04-28 20:31:43 +0300

    exercitiul 2

--- Exercises/02 Modifying Content/app.js
+++ Exercises/02 Modifying Content/app.js
@@ -1,1 +1,12 @@
-// JavaScript code
+/*1. Change the text content of a <h1> element to "Hello, World!" using JavaScript.
+  2. Append a new <li> element to an existing <ul> list on a webpage.*/
+
+const container = document.querySelector(".container");
+const h1 = document.createElement("h1");
+h1.innerHTML = "Hello, World!";
+container.appendChild(h1);
+
+const element = document.createElement("li");
+element.innerHTML = "Element4 - nou";
+const lista = document.querySelector("ul");
+lista.appendChild(element);
