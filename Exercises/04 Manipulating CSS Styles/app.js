dac180ea86293a6107ac8d3f935efb467e0c57b0
Sandina Ciungu <cornelia.ocheana@farmexim.ro>
2023-04-29 13:46:45 +0300

    exercitiul 4

--- Exercises/04 Manipulating CSS Styles/app.js
+++ Exercises/04 Manipulating CSS Styles/app.js
@@ -1,1 +1,15 @@
-// JavaScript code
+/*1. Toggle the background color of a <div> element between red and blue when clicked.
+  2. Change the font size of a paragraph dynamically based on a user-selected value from a dropdown menu*/
+
+const fundal = document.querySelector(".container");
+
+fundal.style.backgroundColor = "blue";
+fundal.style.height = "100vh";
+fundal.style.width = "100vh";
+
+fundal.addEventListener("click", changeColor);
+
+function changeColor() {
+  fundal.style.backgroundColor =
+    fundal.style.backgroundColor == "red" ? "blue" : "red";
+}
