65fddd62510c68eb34989d5aa6f13e59585ce0b1
Sandina Ciungu <cornelia.ocheana@farmexim.ro>
2023-04-29 07:52:15 +0300

    exercitiul 3

--- Exercises/03 Modifying Attributes/app.js
+++ Exercises/03 Modifying Attributes/app.js
@@ -1,1 +1,12 @@
-// JavaScript code
+/*1. Add a target="\_blank" attribute to a hyperlink to open it in a new tab when clicked.
+  2. Change the source (src) attribute of an <img> element to display a different image.*/
+
+const link = document.querySelector("a");
+link.setAttribute("target", "_blank");
+
+const poza = document.querySelector("img");
+poza.setAttribute(
+  "src",
+  "https://www.supercoloring.com/sites/default/files/styles/coloring_medium/public/cif/2019/10/dog-coloring-page.png"
+);
+poza.setAttribute("alt", "caine");
