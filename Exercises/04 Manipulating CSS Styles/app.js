4697da0a718359d8dfc222ddfe82a2277897ff11
Sandina Ciungu <cornelia.ocheana@farmexim.ro>
2023-05-01 09:52:36 +0300

    Exercitiul 4

--- Exercises/04 Manipulating CSS Styles/app.js
+++ Exercises/04 Manipulating CSS Styles/app.js
@@ -4,6 +4,7 @@
 const fundal = document.querySelector(".container");
 
 fundal.style.backgroundColor = "blue";
+fundal.style.fontSize = "10px";
 fundal.style.height = "100vh";
 fundal.style.width = "100vh";
 
@@ -12,4 +13,15 @@ fundal.addEventListener("click", changeColor);
 function changeColor() {
   fundal.style.backgroundColor =
     fundal.style.backgroundColor == "red" ? "blue" : "red";
+}
+
+const paragraf = document.querySelector("p");
+
+const dimensiune = document.querySelector("select");
+dimensiune.addEventListener("change", schimbaFont);
+
+function schimbaFont() {
+  paragraf.style.fontSize = dimensiune.value;
+  //paragraf.setAttribute("style", "font-size:" + dimensiune.value + ";");
+  //console.log(paragraf.getAttribute("style", "font-size"));
 }
