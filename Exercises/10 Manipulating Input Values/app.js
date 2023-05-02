f1721c3fc3a770c7fddc3d09b30edbb049d3ddaa
Sandina Ciungu <cornelia.ocheana@farmexim.ro>
2023-05-02 20:12:20 +0300

    Exercitiul 10

--- Exercises/10 Manipulating Input Values/app.js
+++ Exercises/10 Manipulating Input Values/app.js
@@ -1,2 +1,25 @@
 /*1. Calculate the sum of two input fields and display the result in a separate <span> element when a button is clicked.
   2. Clear the values of all input fields on a form when a "Reset" button is clicked.*/
+
+const butonRefresh = document.querySelector("#buton1");
+butonRefresh.addEventListener("click", recalculeaza);
+
+var suma = document.querySelector("span");
+
+function recalculeaza() {
+  suma.innerHTML =
+    "Suma celor 2 numere este: " +
+    Number(
+      Number(document.getElementById("numarul1").value) +
+        Number(document.getElementById("numarul2").value)
+    );
+}
+
+const butonReset = document.querySelector("#buton2");
+butonReset.addEventListener("click", reset);
+
+function reset() {
+  document.getElementById("numarul1").value = "";
+  document.getElementById("numarul2").value = "";
+  document.querySelector("span").innerHTML = "Suma celor 2 numere este: ";
+}
