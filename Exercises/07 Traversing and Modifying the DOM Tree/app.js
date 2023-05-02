f56eae17a477bb912ad3425c9711d00ebec5bf8d
Sandina Ciungu <cornelia.ocheana@farmexim.ro>
2023-05-02 18:35:12 +0300

    Exercitiul 7

--- Exercises/07 Traversing and Modifying the DOM Tree/app.js
+++ Exercises/07 Traversing and Modifying the DOM Tree/app.js
@@ -1,1 +1,47 @@
-// JavaScript code
+/*1. Traverse the DOM tree and find the parent element of a specific element and change its background color.
+  2. Remove all child elements from a container <div> using JavaScript.*/
+
+const listaTaguri = document.querySelector("#lista-finala");
+const listaFinala = document.querySelectorAll(".lista");
+for (let i = 0; i < listaFinala.length - 1; i++) {
+  var el = document.createElement("option");
+  el.textContent = listaFinala[i].tagName;
+  el.value = listaFinala[i].tagName;
+  listaTaguri.appendChild(el);
+}
+
+const selectieCrt = document.getElementById("lista-finala");
+const buton = document.querySelector("button.lista-finala");
+buton.addEventListener("click", schimbaBackground);
+
+function schimbaBackground() {
+  document.querySelector(selectieCrt.value + ".lista").style.backgroundColor =
+    "pink";
+}
+
+const buton2 = document.querySelector("button.sterge-tot");
+buton2.addEventListener("click", stergeTot);
+
+const divContainer = document.querySelector("div:nth-of-type(1)");
+
+function stergeTot() {
+  /*
+  const listaTot = document.querySelectorAll(".lista");
+  for (let i = 0; i < listaTot.length - 1; i++) {
+    listaTot[i].remove();
+  }
+  const listaTot2 = document.querySelectorAll(".schimbaProprietate");
+  for (let i = 0; i < listaTot2.length - 1; i++) {
+    listaTot2[i].remove();
+  }
+  const listaTot3 = document.querySelectorAll(".lista-finala");
+  for (let i = 0; i < listaTot3.length - 1; i++) {
+    listaTot3[i].remove();
+  }
+  buton.remove();
+*/
+
+  while (divContainer.firstChild) {
+    divContainer.removeChild(divContainer.firstChild);
+  }
+}
